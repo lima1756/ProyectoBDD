597498b27e91dcad96b5fab286427829c4f5c212
BRSADT <brendasamant@hotmail.com>
2016-06-11 17:47:36 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Class-Functions/BaseDeDatos.php
    #	JavaScripts/validacionRegistro.js
    #	registroK.php
    
    lean el comentario
    
    En teoria quite la validacion de contraseña por que
    no me aceptaba
    ninguna y no podia registrarme, asi que debemos volverla
    a poner, pero
    esto fue para ver si ya se puede agregar usuarios a la bdd
    y sip, ya se
    puede :D

--- bddNoFake/proyecto.sql
+++ bddNoFake/proyecto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.6.2
+-- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2016 a las 05:44:27
--- Versión del servidor: 5.6.17
--- Versión de PHP: 5.5.12
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2016 at 06:22 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,114 +14,115 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `proyecto`
+-- Database: `proyecto`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `registroUser` (IN `nombre` VARCHAR(30), IN `apellido` VARCHAR(30), IN `pass` VARCHAR(20), IN `usuario` VARCHAR(10), IN `edad` DATE, IN `correo` VARCHAR(30))  INSERT INTO persona(Nombre, Apellido, Pass, Usuario, Edad, email) values (nombre, apellido, pass, usuario, edad, correo)$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `agenda`
+-- Table structure for table `agenda`
 --
 
-CREATE TABLE IF NOT EXISTS `agenda` (
+CREATE TABLE `agenda` (
   `Fecha_inicio` datetime NOT NULL,
-  `id_Registro` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Fecha_fin` datetime NOT NULL,
-  PRIMARY KEY (`id_Registro`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_Registro` int(10) UNSIGNED NOT NULL,
+  `Fecha_fin` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asiento`
+-- Table structure for table `asiento`
 --
 
-CREATE TABLE IF NOT EXISTS `asiento` (
-  `id_Zona` int(10) unsigned NOT NULL,
-  `Fila` char(1) NOT NULL,
-  `Numero` int(10) unsigned NOT NULL,
-  `id_Asiento` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id_Asiento`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `asiento` (
+  `id_Zona` int(10) UNSIGNED NOT NULL,
+  `Fila` char(1) COLLATE utf8_unicode_ci NOT NULL,
+  `Numero` int(10) UNSIGNED NOT NULL,
+  `id_Asiento` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `boleto`
+-- Table structure for table `boleto`
 --
 
-CREATE TABLE IF NOT EXISTS `boleto` (
-  `id_Boleto` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Folio_Compra` int(10) unsigned NOT NULL,
-  `Descripcion` varchar(255) NOT NULL,
-  `id_Asiento` int(10) unsigned NOT NULL,
-  `id_Persona` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id_Boleto`),
-  UNIQUE KEY `Folio_Compra` (`Folio_Compra`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `boleto` (
+  `id_Boleto` int(10) UNSIGNED NOT NULL,
+  `Folio_Compra` int(10) UNSIGNED NOT NULL,
+  `Descripcion` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id_Asiento` int(10) UNSIGNED NOT NULL,
+  `id_Persona` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `concierto`
+-- Table structure for table `concierto`
 --
 
-CREATE TABLE IF NOT EXISTS `concierto` (
-  `Nombre` varchar(20) NOT NULL,
-  `Descripcion` varchar(20) NOT NULL,
-  `id_Concierto` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Artista` varchar(20) NOT NULL,
-  `Genero` varchar(20) NOT NULL,
-  `img` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_Concierto`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `concierto` (
+  `Nombre` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `Descripcion` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `id_Concierto` int(10) UNSIGNED NOT NULL,
+  `Artista` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `Genero` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `img` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `de`
+-- Table structure for table `de`
 --
 
-CREATE TABLE IF NOT EXISTS `de` (
-  `id_Zona` int(10) unsigned NOT NULL,
-  `id_Concierto` int(10) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `de` (
+  `id_Zona` int(10) UNSIGNED NOT NULL,
+  `id_Concierto` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `guardado_en`
+-- Table structure for table `guardado_en`
 --
 
-CREATE TABLE IF NOT EXISTS `guardado_en` (
-  `id_Concierto` int(10) unsigned NOT NULL,
-  `id_Registro` int(10) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `guardado_en` (
+  `id_Concierto` int(10) UNSIGNED NOT NULL,
+  `id_Registro` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `persona`
+-- Table structure for table `persona`
 --
 
-CREATE TABLE IF NOT EXISTS `persona` (
+CREATE TABLE `persona` (
   `Edad` date NOT NULL,
-  `Pass` varchar(20) NOT NULL,
-  `Usuario` varchar(10) NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Apellido` varchar(30) NOT NULL,
-  `Id_Persona` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(30) NOT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id_Persona`),
-  UNIQUE KEY `Usuario` (`Usuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `Pass` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `Usuario` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `Nombre` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `Apellido` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `Id_Persona` int(10) UNSIGNED NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Volcado de datos para la tabla `persona`
+-- Dumping data for table `persona`
 --
 
 INSERT INTO `persona` (`Edad`, `Pass`, `Usuario`, `Nombre`, `Apellido`, `Id_Persona`, `email`, `admin`) VALUES
@@ -129,35 +130,122 @@ INSERT INTO `persona` (`Edad`, `Pass`, `Usuario`, `Nombre`, `Apellido`, `Id_Pers
 ('2016-06-08', '213213', '21321', '132132', '13213231', 2, '213213', 0),
 ('2016-06-10', '$email', 'SAD', 'SADAAS', 'SADASD', 5, 'brendasamant@hotmail.com', 0),
 ('2016-06-11', '36606460', 'clau', 'claudia', 'de la torre', 6, 'cyberclaudia_3@hotmail.com', 0),
-('2016-06-13', '123456789', 'greenD', 'billie', 'armstrong', 7, 'billiejoe_armstrong@live.com.m', 0);
+('2016-06-13', '123456789', 'greenD', 'billie', 'armstrong', 7, 'billiejoe_armstrong@live.com.m', 0),
+('1998-01-07', 'LIMA.mali98', 'lima1756', 'Luis Iván', 'Morett Arévalo', 21, 'lima@gmail.com', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `recibo`
+-- Table structure for table `recibo`
 --
 
-CREATE TABLE IF NOT EXISTS `recibo` (
-  `Forma_Pago` varchar(20) NOT NULL,
+CREATE TABLE `recibo` (
+  `Forma_Pago` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `Fecha` date NOT NULL,
-  `Banco` varchar(20) NOT NULL,
-  `Folio_Compra` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`Folio_Compra`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `Banco` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `Folio_Compra` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `zona`
+-- Table structure for table `zona`
 --
 
-CREATE TABLE IF NOT EXISTS `zona` (
-  `Precio` int(10) unsigned NOT NULL,
-  `id_Zona` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Ubicacion` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_Zona`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `zona` (
+  `Precio` int(10) UNSIGNED NOT NULL,
+  `id_Zona` int(10) UNSIGNED NOT NULL,
+  `Ubicacion` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agenda`
+--
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`id_Registro`);
+
+--
+-- Indexes for table `asiento`
+--
+ALTER TABLE `asiento`
+  ADD PRIMARY KEY (`id_Asiento`);
+
+--
+-- Indexes for table `boleto`
+--
+ALTER TABLE `boleto`
+  ADD PRIMARY KEY (`id_Boleto`),
+  ADD UNIQUE KEY `Folio_Compra` (`Folio_Compra`);
+
+--
+-- Indexes for table `concierto`
+--
+ALTER TABLE `concierto`
+  ADD PRIMARY KEY (`id_Concierto`);
+
+--
+-- Indexes for table `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`Id_Persona`),
+  ADD UNIQUE KEY `Usuario` (`Usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `recibo`
+--
+ALTER TABLE `recibo`
+  ADD PRIMARY KEY (`Folio_Compra`);
+
+--
+-- Indexes for table `zona`
+--
+ALTER TABLE `zona`
+  ADD PRIMARY KEY (`id_Zona`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agenda`
+--
+ALTER TABLE `agenda`
+  MODIFY `id_Registro` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `asiento`
+--
+ALTER TABLE `asiento`
+  MODIFY `id_Asiento` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `boleto`
+--
+ALTER TABLE `boleto`
+  MODIFY `id_Boleto` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `concierto`
+--
+ALTER TABLE `concierto`
+  MODIFY `id_Concierto` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `Id_Persona` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `recibo`
+--
+ALTER TABLE `recibo`
+  MODIFY `Folio_Compra` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `zona`
+--
+ALTER TABLE `zona`
+  MODIFY `id_Zona` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- iniciark.php
+++ iniciark.php
@@ -14,7 +14,7 @@
 <input id="boton" type="submit" name="boton" value="&rarr;"  style="padding:0px 10px;">
 <input type="text" name="search" placeholder="Buscar..." id="search">
 </form>
-<img src="koncert.jpg" width="200px;">
+<img src="images/koncert.jpg" width="200px;">
 
 </div>
 
