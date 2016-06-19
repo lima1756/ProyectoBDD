8dc31ec6d9d50d68f3c3d5820e8f29d698324248
lima1756 <luisivanmorett@gmail.com>
2016-06-19 11:42:32 -0500

    correccion de errores de diseño
    :3

--- compra.php
+++ compra.php
@@ -50,6 +50,44 @@
 		</div>
 </header>
 
+<div id="sidebar">
+<?php session_start(); ?>
+<?php if(!isset($_SESSION['name'])): ?>
+    <a  href="registroK.php">
+            <div id="btn">
+                <p>Registrate</p>
+            </div>
+        </a>
+    <a href="iniciarK.php">
+        <div id="btn">
+            <p>Iniciar sesion</p>
+        </div>
+    </a>
+<?php else: ?>
+<p class="session">Bienvenido, <?php echo $_SESSION['name']?></p>
+    <?php if(!$_SESSION['adm']): ?>
+        <a href="myTickets.php">
+        <div id="btn">
+            <p>Ver tus boletos</p>
+        </div>
+		  <div id="btn">
+		  <a href="pago.php">
+            <p>pagar o como se iame</p>
+        </div>
+    <?php else: ?>
+        <a href="nuevoConcierto.php">
+        <div id="btn">
+            <p>Agregar concierto</p>
+        </div>
+        </a>
+	<?php endif; ?>
+    <a href="Class-Functions/LogOut.php">
+        <div id="btn">
+            <p>Cerrar Sesion</p>
+        </div>
+    </a>
+<?php endif;?>
+</div>
 
 <div class="cuerpo">
     <section>
@@ -86,48 +124,8 @@
 				<input type="hidden" name="var" value="<?php echo $id; ?>">
 				<input type="submit" value="enviar">
 			</form>
-<div id="sidebar">
-<?php session_start(); ?>
-<?php if(!isset($_SESSION['name'])): ?>
-    <a  href="registroK.php">
-            <div id="btn">
-                <p>Registrate</p>
-            </div>
-        </a>
-    <a href="iniciarK.php">
-        <div id="btn">
-            <p>Iniciar sesion</p>
-        </div>
-    </a>
-<?php else: ?>
-<p class="session">Bienvenido, <?php echo $_SESSION['name']?></p>
-    <?php if(!$_SESSION['adm']): ?>
-        <a href="myTickets.php">
-        <div id="btn">
-            <p>Ver tus boletos</p>
-        </div>
-		  <div id="btn">
-		  <a href="pago.php">
-            <p>pagar o como se iame</p>
-        </div>
-    <?php else: ?>
-        <a href="nuevoConcierto.php">
-        <div id="btn">
-            <p>Agregar concierto</p>
-        </div>
-    </a>
-	
-	
-	
-    <?php endif; ?>
-        <a href="Class-Functions/LogOut.php">
-        <div id="btn">
-            <p>Cerrar Sesion</p>
-        </div>
-        </a>
-<?php endif;?>
-    
-</div>
+
+
 
 
 
